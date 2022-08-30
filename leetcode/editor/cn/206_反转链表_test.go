4a2ac6eb810c651ba8b53a094443abfc85a1ccdb
zhou tao <zhoutao@itiger.com>
2022-08-30 20:16:54 +0800

    反转链表

---
+++ leetcode/editor/cn/206_反转链表_test.go
@@ -0,0 +1,31 @@
+package leet
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+		{
+			name: "反转链表",
+			args: args{head: ToLinkedList([]int{1, 2, 3, 4})},
+			want: ToLinkedList([]int{4, 3, 2, 1}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- leetcode/editor/cn/listNode.go
+++ leetcode/editor/cn/listNode.go
@@ -4,3 +4,16 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+func ToLinkedList(arr []int) *ListNode {
+	if len(arr) == 0 {
+		return nil
+	}
+	var head = &ListNode{Val: arr[0]}
+	curr := head
+	for i := 1; i < len(arr); i++ {
+		curr.Next = &ListNode{Val: arr[i]}
+		curr = curr.Next
+	}
+	return head
+}
