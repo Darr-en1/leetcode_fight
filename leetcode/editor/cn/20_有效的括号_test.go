8a791f059462617c9cabf4886a2f2a6e64c73b92
zhou tao <zhoutao@itiger.com>
2022-08-31 17:01:59 +0800

    有效的括号

---
+++ leetcode/editor/cn/20_有效的括号_test.go
@@ -0,0 +1,32 @@
+package leet
+
+import "testing"
+
+func Test_isValid(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "有效的括号",
+			args: args{s: "{[]}"},
+			want: true,
+		},
+		{
+			name: "有效的括号",
+			args: args{s: "["},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValid(tt.args.s); got != tt.want {
+				t.Errorf("isValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
