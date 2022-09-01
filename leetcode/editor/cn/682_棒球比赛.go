76ccb15cc3a6db14fea74e761e786527aee6ca9b
zhou tao <zhoutao@itiger.com>
2022-09-01 18:58:36 +0800

    棒球运动 不用再次遍历

--- leetcode/editor/cn/682_棒球比赛.go
+++ leetcode/editor/cn/682_棒球比赛.go
@@ -96,4 +96,28 @@ func calPoints(ops []string) int {
 	return sum
 }
 
+// 不用再次遍历
+func calPoints1(ops []string) (ans int) {
+	points := []int{}
+	for _, op := range ops {
+		n := len(points)
+		switch op[0] {
+		case '+':
+			ans += points[n-1] + points[n-2]
+			points = append(points, points[n-1]+points[n-2])
+		case 'D':
+			ans += points[n-1] * 2
+			points = append(points, 2*points[n-1])
+		case 'C':
+			ans -= points[n-1]
+			points = points[:len(points)-1]
+		default:
+			pt, _ := strconv.Atoi(op)
+			ans += pt
+			points = append(points, pt)
+		}
+	}
+	return
+}
+
 //leetcode submit region end(Prohibit modification and deletion)
