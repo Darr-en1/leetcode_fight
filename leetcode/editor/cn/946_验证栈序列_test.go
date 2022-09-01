e5120504e208156510a73a0af55c06b7548608d8
zhou tao <zhoutao@itiger.com>
2022-09-01 18:59:14 +0800

    验证栈序列

---
+++ leetcode/editor/cn/946_验证栈序列_test.go
@@ -0,0 +1,31 @@
+package leet
+
+import "testing"
+
+func Test_validateStackSequences(t *testing.T) {
+	type args struct {
+		pushed []int
+		popped []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "验证栈序列",
+			args: args{
+				pushed: []int{1, 2, 3, 4, 5},
+				popped: []int{4, 5, 3, 2, 1},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validateStackSequences(tt.args.pushed, tt.args.popped); got != tt.want {
+				t.Errorf("validateStackSequences() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
